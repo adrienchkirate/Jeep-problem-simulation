913e515b65ca03e190d7804dd2fcd9d4d5c3885f
LexAdrim <adrien.chkirate@gmail.com>
2017-01-18 14:02:09 +0100

    create refreshLine() w/ little change

--- web/js/event.js
+++ web/js/event.js
@@ -2,7 +2,7 @@ var arrayPoint   = ['Réserve', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'
 
 for (var i = 0; i < 26; i++)
 {
-    if(arrayPoint[i] == 'A') {
+    if(arrayPoint[i] == 'Réserve') {
         arrayPoint[i] = {
             name: arrayPoint[i],
             bidon: parseInt($('.reserve').text())
@@ -12,40 +12,69 @@ for (var i = 0; i < 26; i++)
     {
         arrayPoint[i] = {
             name: arrayPoint[i],
-            bidon: 1
+            bidon: 0
         };
     }
 }
 
+function refreshLine()
+{
+    var distance = parseInt($('.distance').text());
+
+
+    $('.ligne').empty();
+    $('.ligne').append('<p style="float: right">Bidon au point</p> <p>Point</p>')
+
+    for(var i=0; i < arrayPoint.length; i++)
+    {
+
+        if(arrayPoint[i].name == arrayPoint[distance].name)
+        {
+            $('.ligne').append('<span class="label label-info" style="float: right">' + arrayPoint[i].bidon + '</span> <span class="label label-default">' + arrayPoint[i].name + '</span>  <span class="label label-danger">Vous êtes ici</span> <br />');
+        }
+        else
+        {
+            $('.ligne').append('<span class="label label-info" style="float: right">' + arrayPoint[i].bidon + '</span> <span class="label label-default">' + arrayPoint[i].name + '</span> <br />');
+        }
+    }
+}
+
+
+refreshLine();
+
 $(document).on('click', '.forward', function() {
 
-    var distance = parseInt($('.distance').text()), fuelUnit = parseInt($('.fuelUnit').text());
+    var distance = parseInt($('.distance').text()), reservoir = $('.reservoir').text();
 
-    if(fuelUnit != 0)
+    if(reservoir != 'Vide')
     {
         $('.currentBidon').text(arrayPoint[distance + 1].bidon);
-        $('.fuelUnit').text(fuelUnit - 1);
+        $('.reservoir').text('Vide');
         $('.currentPoint').text(arrayPoint[distance + 1].name);
         $('.distance').text(distance + 1);
     }
+
+    refreshLine();
 });
 
 $(document).on('click', '.backward', function() {
 
-    var distance = parseInt($('.distance').text()), fuelUnit = parseInt($('.fuelUnit').text());
+    var distance = parseInt($('.distance').text()), reservoir = $('.reservoir').text();
 
-    if(fuelUnit != 0 && arrayPoint[distance].name != 'A')
+    if(reservoir != 'Vide' && arrayPoint[distance].name != 'Réserve')
     {
         $('.currentBidon').text(arrayPoint[distance - 1].bidon);
-        $('.fuelUnit').text(fuelUnit - 1);
+        $('.reservoir').text('Vide');
         $('.currentPoint').text(arrayPoint[distance - 1].name);
         $('.distance').text(distance - 1);
     }
+
+    refreshLine();
 });
 
 $(document).on('click', '.take', function() {
 
-    var distance = parseInt($('.distance').text()), fuelUnit = parseInt($('.fuelUnit').text()), bidonT = parseInt($('.bidonT').text()) ;
+    var distance = parseInt($('.distance').text()), reservoir = $('.reservoir').text(), bidonT = parseInt($('.bidonT').text()) ;
 
     if(arrayPoint[distance].bidon != 0 && bidonT < parseInt($('.capacity').text()))
     {
@@ -53,6 +82,8 @@ $(document).on('click', '.take', function() {
         arrayPoint[distance].bidon -= 1;
         $('.currentBidon').text(arrayPoint[distance].bidon);
     }
+
+    refreshLine();
 });
 
 
@@ -66,15 +97,19 @@ $(document).on('click', '.drop', function() {
         $('.bidonT').text(bidonT - 1);
         $('.currentBidon').text(arrayPoint[distance].bidon);
     }
+
+    refreshLine();
 });
 
 $(document).on('click', '.use', function() {
 
-    var fuelUnit = parseInt($('.fuelUnit').text()), bidonT = parseInt($('.bidonT').text());
+    var reservoir = $('.reservoir').text(), bidonT = parseInt($('.bidonT').text());
 
-    if(bidonT != 0)
+    if(bidonT != 0 && reservoir == 'Vide')
     {
         $('.bidonT').text(bidonT - 1);
-        $('.fuelUnit').text(fuelUnit + 1);
+        $('.reservoir').text('Plein');
     }
+
+    refreshLine();
 });
